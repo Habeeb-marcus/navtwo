d6fb78fa76b683ff3ed39e726c8665cbb78c815c
mac <h.makusota@outlook.com>
2021-10-19 17:15:22 +0100

    mobile submenu positioning issue

---
+++ app.js
@@ -0,0 +1,12 @@
+const bar = document.querySelector('.bar')
+const menu = document.querySelector('.menu');
+
+const displayMenu = () => {
+//  console.log('menu showed');
+menu.classList.add('')
+
+
+}
+
+
+bar.addEventListener('click', displayMenu)
