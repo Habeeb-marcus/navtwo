fcd807e8b67a54dda9cbf3cdde1f51b0cda38bf7
mac <h.makusota@outlook.com>
2021-10-21 10:01:01 +0100

    submenu mobile and toggle function added

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const bar = document.querySelector('.bar')
 const menu = document.querySelector('.menu');
+const nav = document.getElementsByTagName('nav')
 
-const displayMenu = () => {
+
+let menuBar = false
+const toggleMenu = () => {
 //  console.log('menu showed');
-menu.classList.add('')
 
+if(menu.classList.contains('show')){
+menu.classList.remove('show') ;
+
+} else {
+menu.classList.add('show')}
 
 }
 
 
-bar.addEventListener('click', displayMenu)
+
+
+
+bar.addEventListener('click', toggleMenu)
